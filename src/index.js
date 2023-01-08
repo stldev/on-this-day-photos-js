2799ac6ae06477d2614ae2453283e7a9d3b96512
rbb <rick1stl@gmail.com>
2023-01-08 09:09:11 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -33,23 +33,23 @@ function sendEmail(link, email, count) {
 
 async function start() {
   for await (const person of persons) {
+    // TODO: fix paths
     // const destDir = `${fileShare}\\${appScope}\\${person.name}\\${today}`;
     const destDir = `C:\\_CODE\\_STLDEV\\on-this-day-photos-js\\test-media\\rbb-imgs\\${today}`;
 
     if (!existsSync(destDir)) mkdirSync(destDir);
 
+    // TODO: fix paths
     // const destDir2 = `${fileShare}\\${appScope}\\${person.name}`;
     const destDir2 = `C:\\_CODE\\_STLDEV\\on-this-day-photos-js\\test-media\\${today}`;
 
     const dotExts = exts.map((m) => `.${m}`);
     const extsJson = JSON.stringify(dotExts);
-    const month = Number(today.split("-")[1]) + 3;
-    const day = Number(today.split("-")[2]) + 2;
+    const month = Number(today.split("-")[1]) + 3; // TODO: put back to normal
+    const day = Number(today.split("-")[2]) + 2; // TODO: put back to normal
 
     const scriptArgs = `-personsrcpath "${destDir2}" -exts '${extsJson}' -dtmonth ${month} -dtday ${day}`;
-
     const filesJson = execSync(`${scriptGetFiles} ${scriptArgs}`, execOpts);
-
     const files = JSON.parse(filesJson || "[]");
 
     // console.log("files", files);
